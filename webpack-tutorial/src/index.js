62b71d350588b7daa4aa692659b8496c13f26cad
Yong Jing <gohyongjing@gmail.com>
2022-05-30 13:32:44 +0800

    Remove assets from asset management exercise

--- webpack-tutorial/src/index.js
+++ webpack-tutorial/src/index.js
@@ -1,36 +1,8 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './windowsxp.jpeg';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-//import myName from './myName';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    //element.textContent = myName('Yong Jing');
-
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    console.log(Data);
-    console.log(Notes);
-
-    element.appendChild(myIcon);
     return element;
 }
 
